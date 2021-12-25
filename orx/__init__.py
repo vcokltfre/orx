d8261de2377798d4646f427151cf6878c7e04623
vcokltfre <vcokltfre@gmail.com>
2021-12-25 23:28:59 +0000

    feat: add errors

--- orx/__init__.py
+++ orx/__init__.py
@@ -1,3 +1,35 @@
+from .errors import (
+    BadGateway,
+    BadRequest,
+    Forbidden,
+    GatewayTimeout,
+    HTTPError,
+    MethodNotAllowed,
+    NotFound,
+    OrxError,
+    ServerError,
+    ServiceUnavailable,
+    TooManyRequests,
+    Unauthorized,
+    UnprocessableEntity,
+)
+
+__all__ = (
+    "BadGateway",
+    "BadRequest",
+    "Forbidden",
+    "GatewayTimeout",
+    "HTTPError",
+    "MethodNotAllowed",
+    "NotFound",
+    "OrxError",
+    "ServerError",
+    "ServiceUnavailable",
+    "TooManyRequests",
+    "Unauthorized",
+    "UnprocessableEntity",
+)
+
 __version__ = "1.0.0"
 __author__ = "vcokltfre"
 __license__ = "MIT"
---
+++ orx/errors.py
@@ -0,0 +1,57 @@
+from aiohttp import ClientResponse
+
+
+class OrxError(Exception):
+    pass
+
+
+class HTTPError(OrxError):
+    def __init__(self, response: ClientResponse, *args) -> None:
+        self.response = response
+        self.status = response.status
+
+        super().__init__(*args)
+
+
+class BadRequest(HTTPError):
+    pass
+
+
+class Unauthorized(BadRequest):
+    pass
+
+
+class Forbidden(BadRequest):
+    pass
+
+
+class NotFound(BadRequest):
+    pass
+
+
+class MethodNotAllowed(BadRequest):
+    pass
+
+
+class UnprocessableEntity(BadRequest):
+    pass
+
+
+class TooManyRequests(BadRequest):
+    pass
+
+
+class ServerError(HTTPError):
+    pass
+
+
+class BadGateway(ServerError):
+    pass
+
+
+class ServiceUnavailable(ServerError):
+    pass
+
+
+class GatewayTimeout(ServerError):
+    pass
