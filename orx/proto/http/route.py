a0dc7296a6a8937cc75211d29a3389cfb0b7b294
vcokltfre <vcokltfre@gmail.com>
2021-12-25 23:35:16 +0000

    fix(proto): add method and url params to RouteProto

--- orx/proto/http/route.py
+++ orx/proto/http/route.py
@@ -3,6 +3,8 @@ from typing import Protocol
 
 class RouteProto(Protocol):
     bucket: str
+    method: str
+    url: str
 
     def __init__(self, method: str, path: str, **params: str | int) -> None:
         ...
