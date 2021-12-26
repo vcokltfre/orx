e278676d21534deaa662418f27e4beee00b0e737
vcokltfre <vcokltfre@gmail.com>
2021-12-26 17:15:54 +0000

    chore(gateway): add classes to __init__.py

---
+++ orx/src/gateway/__init__.py
@@ -0,0 +1,10 @@
+from .client import GatewayClient
+from .event import EventDirection, GatewayEvent
+from .shard import Shard
+
+__all__ = (
+    "GatewayClient",
+    "EventDirection",
+    "GatewayEvent",
+    "Shard",
+)
