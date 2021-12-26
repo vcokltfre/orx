b881decc37d9b455efef2f5518ea1490d76e99bc
vcokltfre <vcokltfre@gmail.com>
2021-12-26 16:24:31 +0000

    feat(objects): add smartrepr mixin

--- orx/objects/object.py
+++ orx/objects/object.py
@@ -2,22 +2,22 @@ from datetime import datetime
 
 from orx.utils import snowflake_time
 
+from .repr import SmartReprMixin
 from .state import ConnectionState
 
 
-class Object:
+class Object(SmartReprMixin):
     """
     Representation of a generic Discord API object.
     """
+
+    __smart_repr__ = ("id",)
 
     def __init__(self, id: int) -> None:
         self.id = id
 
     def __str__(self) -> str:
         return str(self.id)
-
-    def __repr__(self) -> str:
-        return f"<Object id={self.id}>"
 
     @property
     def created_at(self) -> datetime:
@@ -29,9 +29,11 @@ class Object:
 
 
 class StatefulObject(Object):
+    __smart_repr__ = (
+        "id",
+        "state",
+    )
+
     def __init__(self, id: int, state: ConnectionState) -> None:
         super().__init__(id)
         self.state = state
-
-    def __repr__(self) -> str:
-        return f"<StatefulObject id={self.id} state={self.state}>"
